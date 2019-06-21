97be10e74c377ebdbbc6f20ee2a37c76d8fc1dfd
Leslie <rleslie1015@gmail.com>
2019-06-21 11:39:20 -0500

    added service and dao layers to my bookstore

---
+++ StartHere/src/main/java/com/lambdaschool/starthere/repository/SectionRepository.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.starthere.repository;
+
+import com.lambdaschool.starthere.models.Section;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SectionRepository extends CrudRepository<Section, Long>
+{
+
+}
---
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/SectionService.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.starthere.services;
+
+public interface SectionService
+{
+}
